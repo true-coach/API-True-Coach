89ead0b8378ea736e90c1bff1f22af7834757864
Regan Dufort <regan.t.dufort@gmail.com>
2019-02-03 17:53:18 -0800

    Project initial setup

---
+++ TrueCoach/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrueCoach.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
